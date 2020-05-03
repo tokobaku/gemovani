edaf8d8c20132e3ef46e94ab34bb05466f0e105d
tokobaku <tornike.bakuradze@scandiweb.com>
2020-05-03 19:34:38 +0400

    Updated homepage tours

--- resources/app/component/Tour/Tour.component.tsx
+++ resources/app/component/Tour/Tour.component.tsx
@@ -8,6 +8,7 @@ import Image from 'Component/Image';
 import Asset from 'Helper/Asset';
 import { getTranslation } from 'Helper/Translation';
 import __ from 'Helper/__';
+import Link from 'Component/Link';
 
 import 'Component/Tour/Tour.style';
 
@@ -17,15 +18,25 @@ export interface TourProps {
 
 export default class Tour extends React.PureComponent<TourProps> {
     renderTourDetails(): React.ReactNode {
-        const { tour } = this.props;
+        // eslint-disable-next-line @typescript-eslint/camelcase
+        const { tour, tour: { url_key } } = this.props;
+        const translation = getTranslation(tour, 'en');
+        // eslint-disable-next-line @typescript-eslint/camelcase
+        const tourUrl = `/tour/${url_key}`;
 
         return (
             <div block="Tour" elem="TourDetails">
-                <h2 block="Tour" elem="Title">{getTranslation(tour, 'en')?.title}</h2>
+                <h2 block="Tour" elem="Title">{translation?.title}</h2>
                 <dl>
                     <dt block="Tour" elem="Term">Dates</dt>
                     <dd>{__('%s - %s', tour.start_date.toDateString(), tour.end_date.toDateString())}</dd>
                 </dl>
+                <Link
+                    to={tourUrl}
+                    mix={{ block: 'Tour', elem: 'Link' }}
+                >
+                    {__('More')}
+                </Link>
             </div>
         );
     }
