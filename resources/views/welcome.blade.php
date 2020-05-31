19550719c2386d30a691fa1c15286c5c73a11bc1
tokobaku <tornike.bakuradze@scandiweb.com>
2020-05-31 14:51:03 +0400

    Added fb messenger

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,6 +9,25 @@
     </head>
     <body>
         <div id="root"></div>
+        <div id="fb-root"></div>
+        <div class="fb-customerchat"
+             attribution=setup_tool
+             page_id="111269143856601">
+        </div>
         <script async src="{{ asset('application/main.js') }}?version={{ env('VERSION') }}"></script>
+        <script>
+            window.fbAsyncInit = function() {
+                FB.init({
+                    xfbml            : true,
+                    version          : 'v7.0'
+                });
+            };
+            (function(d, s, id) {
+                var js, fjs = d.getElementsByTagName(s)[0];
+                if (d.getElementById(id)) return;
+                js = d.createElement(s); js.id = id;
+                js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+                fjs.parentNode.insertBefore(js, fjs);
+            }(document, 'script', 'facebook-jssdk'));</script>
     </body>
 </html>
